4ce7609786c67c37d77ea1778f2e9616f4bede90
Mateus Pereira da Silva <45442430+mateus2033@users.noreply.github.com>
2026-09-07 16:42:01 -0300

    Estrutura inicial

---
+++ src/test/java/portal/voll/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package portal.voll.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
